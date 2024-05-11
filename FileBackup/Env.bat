9495549e9ac7f1fe945228f0bbbd727977efb1b6
yamdah <150097822+yamdah@users.noreply.github.com>
2024-05-12 03:23:43 +0900

    Create Env.bat

---
+++ FileBackup/Env.bat
@@ -0,0 +1,11 @@
+rem **************************
+rem * 　　環境変数設定　　   *
+rem **************************
+rem 任意のパスを設定する
+
+rem ファイルが出力されるフォルダ
+ set pO=C:\test\folder1
+rem ファイルを編集するフォルダ
+ set pW=C:\test\folder2
+rem ファイルをバックアップするフォルダ
+ set pB=C:\test\folder2
